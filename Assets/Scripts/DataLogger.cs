39725501ccec3c038ceaac968fdaa5753449eda7
Daix18 <danizaguirre10@gmail.com>
2026-02-11 19:13:44 +0100

    Csv system

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -1,24 +1,70 @@
 using StarterAssets;
+using System;
+using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
+
+public struct LogEntry
+{
+    public float Time;
+    public float CameraRotationX;
+    public float CameraRotationY;
+}
+
 
 public class DataLogger : MonoBehaviour
 {
+    //Timers
+    float sampleInterval = 0.2f;
+    float timer = 0f;
+
+    //Data variables
     float gameTime;
-    FirstPersonController playerController;
     public Transform playerCameraRotationX;
     public Transform playerCameraRotationY;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        playerController = GetComponent<FirstPersonController>();
-    }
+
+    //Variable Logger
+    List<LogEntry> logEntries = new List<LogEntry>();
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         gameTime += Time.deltaTime;
+
         float CameraRotationX = playerCameraRotationX.transform.eulerAngles.y;
         float  CameraRotationY = playerCameraRotationY.transform.eulerAngles.x;
-        Debug.Log("Time: " + gameTime + " || " + "Camera Rotation Y: " + CameraRotationY + " || " + "Camera Rotation X: " + CameraRotationX);
+
+        if (timer >= sampleInterval)
+        {
+            LogEntry entry = new LogEntry();
+            entry.Time = gameTime;
+            entry.CameraRotationX = CameraRotationX;
+            entry.CameraRotationY = CameraRotationY;
+
+            logEntries.Add(entry);
+            timer -= sampleInterval ;
+        }
+    }
+
+    void SaveToCSV()
+    {
+        string csv = "Time;CameraRotationX;CameraRotationY\n";
+        string fileName = "Baseline_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv";
+        string filePath = Application.persistentDataPath + "/" + fileName;
+        //Write to CSV string
+        foreach (LogEntry outputentry in logEntries)
+        {
+            csv += outputentry.Time.ToString("F3") + ";" + 
+                outputentry.CameraRotationX + ";" + 
+                outputentry.CameraRotationY + "\n";
+        }
+        File.WriteAllText(filePath, csv);
+        Debug.Log("Data saved to: " + filePath);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveToCSV();
     }
 }
